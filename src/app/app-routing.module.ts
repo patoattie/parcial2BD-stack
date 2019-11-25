ddf66e2c91c1a0d263a7ddfac2810cf48272140b
patoattie <patoattie@gmail.com>
2019-11-25 09:23:01 -0300

    canActivate guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { LoginComponent } from './componentes/login/login.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 
+// Import canActivate guard services
+import { AuthGuard } from "./guard/auth.guard";
+import { SecureInnerPagesGuard } from "./guard/secure-inner-pages.guard";
+
 const routes: Routes = 
 [
-  {path: '', redirectTo: 'Login', pathMatch: 'full'/*, canActivate: [SecureInnerPagesGuard]*/},
-  {path: 'Login' , component: LoginComponent/*, canActivate: [SecureInnerPagesGuard]*/},
-  {path: 'Principal' , component: PrincipalComponent/*, canActivate: [AuthGuard]*/},
-  {path: 'Registro' , component: RegistroComponent/*, canActivate: [SecureInnerPagesGuard]*/},
+  {path: '', redirectTo: 'Login', pathMatch: 'full', canActivate: [SecureInnerPagesGuard]},
+  {path: 'Login' , component: LoginComponent, canActivate: [SecureInnerPagesGuard]},
+  {path: 'Principal' , component: PrincipalComponent, canActivate: [AuthGuard]},
+  {path: 'Registro' , component: RegistroComponent, canActivate: [SecureInnerPagesGuard]},
   {path: '**' , component: LoginComponent},
   {path: 'error' , component: LoginComponent}
 ];
