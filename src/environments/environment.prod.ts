aabcf1c5a710545e18854423551d3dbe93716977
patoattie <patoattie@gmail.com>
2019-11-25 17:43:22 -0300

    tratando de deployar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAXFSL9K9c7oiG6X2oEhaQ0ho6au483G1c',
+    authDomain: 'parcial2bd.firebaseapp.com',
+    databaseURL: 'https://parcial2bd.firebaseio.com',
+    projectId: 'parcial2bd',
+    storageBucket: 'parcial2bd.appspot.com',
+    messagingSenderId: '673305199422',
+    appId: '1:673305199422:web:9c1827d7db58200633226f'
+  }
 };
