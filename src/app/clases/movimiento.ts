e8b6ba663cdc4e595dbc0dc53cb3e0b41d6d0eb2
patoattie <patoattie@gmail.com>
2019-12-02 17:05:39 -0300

    listado de movimientos

--- src/app/clases/movimiento.ts
+++ src/app/clases/movimiento.ts
@@ -1,4 +1,21 @@
+import {EPerfil} from '../enums/eperfil.enum';
+import {ESucursal} from '../enums/esucursal.enum';
+import {ETipo} from '../enums/etipo.enum';
+
 export class Movimiento 
 {
-    
-}
+    public idCollection: string;
+    public uid: string;
+    public tipo: ETipo;
+    public fecha: string;
+    public cantidad: number;
+    public detalle: string;
+    public idProducto: string;
+    public codigoProducto: string;
+    public nombreProducto: string;
+    public descripcionProducto: string;
+    public costoProducto: number;
+    public idUsuario: string;
+    public perfilUsuario: EPerfil;
+    public sucursalUsuario: ESucursal;
+  }
---
+++ src/app/enums/etipo.enum.ts
@@ -0,0 +1,5 @@
+export enum ETipo 
+{
+  Alta = 'Alta',
+  Baja = 'Baja'
+}
