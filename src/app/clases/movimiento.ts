92607b6acce9c6a529280c91432ccb1b8ed55b64
patoattie <patoattie@gmail.com>
2019-12-03 17:12:49 -0300

    listado de movimientos

--- src/app/clases/movimiento.ts
+++ src/app/clases/movimiento.ts
@@ -4,18 +4,54 @@ import {ETipo} from '../enums/etipo.enum';
 
 export class Movimiento 
 {
-    public idCollection: string;
-    public uid: string;
-    public tipo: ETipo;
-    public fecha: string;
-    public cantidad: number;
-    public detalle: string;
-    public idProducto: string;
-    public codigoProducto: string;
-    public nombreProducto: string;
-    public descripcionProducto: string;
-    public costoProducto: number;
-    public idUsuario: string;
-    public perfilUsuario: EPerfil;
-    public sucursalUsuario: ESucursal;
+  public idCollection: string;
+  public uid: string;
+  public tipo: ETipo;
+  public sucursal: ESucursal;
+  public fecha: string;
+  public cantidad: number;
+  public detalle: string;
+  public idProducto: string;
+  public codigoProducto: string;
+  public nombreProducto: string;
+  public descripcionProducto: string;
+  public costoProducto: number;
+  public idUsuario: string;
+  public perfilUsuario: EPerfil;
+  public sucursalUsuario: ESucursal;
+
+  constructor(
+    tipo?: ETipo,
+    sucursal?: ESucursal,
+    fecha?: string,
+    cantidad?: number,
+    detalle?: string,
+    idProducto?: string,
+    codigoProducto?: string,
+    nombreProducto?: string,
+    descripcionProducto?: string,
+    costoProducto?: number,
+    idUsuario?: string,
+    perfilUsuario?: EPerfil,
+    sucursalUsuario?: ESucursal,
+    idCollection?: string,
+    uid?: string
+  )
+  {
+    this.tipo = tipo;
+    this.sucursal = sucursal;
+    this.fecha = fecha;
+    this.cantidad = cantidad;
+    this.detalle = detalle;
+    this.idProducto = idProducto;
+    this.codigoProducto = codigoProducto;
+    this.nombreProducto = nombreProducto;
+    this.descripcionProducto = descripcionProducto;
+    this.costoProducto = costoProducto;
+    this.idUsuario = idUsuario;
+    this.perfilUsuario = perfilUsuario;
+    this.sucursalUsuario = sucursalUsuario;
+    this.idCollection = idCollection;
+    this.uid = uid;
   }
+}
---
+++ src/app/servicios/movimientos.service.ts
@@ -0,0 +1,153 @@
+import { Injectable } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
+import { AngularFireStorage } from "@angular/fire/storage";
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+import { Movimiento } from '../clases/movimiento';
+import { Usuario } from '../clases/usuario';
+import {AuthService} from './auth.service';
+import {UsuariosService} from './usuarios.service';
+
+import * as firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovimientosService 
+{
+  private movimientos: Observable<Movimiento[]>;
+  private movimientoCollection: AngularFirestoreCollection<any>;
+  public muestraAbm: boolean;
+
+  constructor(
+    private afs: AngularFirestore, 
+    private authService: AuthService, 
+    private usuariosService: UsuariosService, 
+    public storage: AngularFireStorage, 
+    public pipe: DatePipe)
+  {
+    this.movimientoCollection = this.afs.collection<any>('movimientos');
+    this.movimientos = this.movimientoCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const idCollection = a.payload.doc.id;
+          return { idCollection, ...data };
+        });
+      })
+    );
+    this.muestraAbm = false;
+  }
+
+  public getMovimientos(): Observable<Movimiento[]>
+  {
+    return this.movimientos;
+  }
+
+  /*public getMovimiento(codigo: string): Movimiento
+  {
+    let retorno: Movimiento = null;
+
+    this.getMovimientos().forEach((arrMovimientos) =>
+    {
+      arrMovimientos.forEach(unMovimiento =>
+      {
+        if(unMovimiento.codigo == codigo)
+        {
+          retorno = unMovimiento;
+        }
+      });
+    });
+
+    return retorno;
+  }*/
+
+  public getMovimientoPorId(idCollection: string): Observable<Movimiento> 
+  {
+    return this.movimientoCollection.doc<any>(idCollection).valueChanges().pipe(
+      take(1),
+      map(movimiento => {
+        movimiento.idCollection = idCollection;
+        return movimiento
+      })
+    );
+  }
+
+  public addMovimiento(movimiento: Movimiento): Promise<void | DocumentReference> 
+  {
+    return this.movimientoCollection.add(
+    {
+      tipo: movimiento.tipo,
+      sucursal: movimiento.sucursal,
+      fecha: movimiento.fecha,
+      cantidad: movimiento.cantidad,
+      detalle: movimiento.detalle,
+      idProducto: movimiento.idProducto,
+      codigoProducto: movimiento.codigoProducto,
+      nombreProducto: movimiento.nombreProducto,
+      descripcionProducto: movimiento.descripcionProducto,
+      costoProducto: movimiento.costoProducto
+    })
+    .then((doc) =>
+    {
+      this.SetData(doc);
+    });
+  }
+ 
+  public updateMovimiento(movimiento: Movimiento): Promise<void> 
+  {
+    return this.movimientoCollection.doc(movimiento.idCollection).update(
+    {
+      tipo: movimiento.tipo,
+      sucursal: movimiento.sucursal,
+      fecha: movimiento.fecha,
+      cantidad: movimiento.cantidad,
+      detalle: movimiento.detalle,
+      idProducto: movimiento.idProducto,
+      codigoProducto: movimiento.codigoProducto,
+      nombreProducto: movimiento.nombreProducto,
+      descripcionProducto: movimiento.descripcionProducto,
+      costoProducto: movimiento.costoProducto
+    });
+  }
+ 
+  public deleteMovimiento(idCollection: string): Promise<void> 
+  {
+    return this.movimientoCollection.doc(idCollection).delete();
+  }
+
+  public SetData(movimiento: DocumentReference)
+  {
+    let usuarioUID: string = this.authService.getUid();
+    let usuario: Usuario = this.usuariosService.getUsuario(usuarioUID);
+    const movimientoRef: AngularFirestoreDocument<any> = this.afs.doc(`movimientos/${movimiento.id}`);
+    const movimientoData = {
+      idCollection: movimiento.id,
+      uid: usuarioUID,
+      idUsuario: usuario.idCollection,
+      perfilUsuario: usuario.perfil,
+      sucursalUsuario: usuario.sucursal
+    };
+    return movimientoRef.set(movimientoData, {
+      merge: true
+    });
+  }
+
+  /*public SetURL(movimiento: DocumentReference, fotoURL: string)
+  {
+    const movimientoRef: AngularFirestoreDocument<any> = this.afs.doc(`movimientos/${movimiento.id}`);
+    const movimientoData = {
+      photoURL: fotoURL
+    };
+    return movimientoRef.set(movimientoData, {
+      merge: true
+    });
+  }*/
+
+  public getFecha(): string
+  {
+    return new DatePipe('en-US').transform(Date.now(), 'yyyyMMddHHmmssSSS', '-0300');
+  }
+}
